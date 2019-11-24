dc72693850f8f600a1a92de4da05203ec06df688
Edward Burke <empburke@outlook.com>
2019-11-24 00:48:10 +0000

    Added a network manager with photon code -There is now a functional connect screen that goes to the main menu after connecting the client

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers = 10;
+    public TextMeshProUGUI connectionStatusText;
+    public GameObject serverConnectScreen;
+    public GameObject mainMenu;
+
+    public static NetworkManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        BeginConnecting();
+    }
+
+    public void BeginConnecting()
+    {
+        if (Input.anyKeyDown && !PhotonNetwork.IsConnected)
+        {
+            print("Key pressed");
+            connectionStatusText.SetText("Connecting to server...");
+            ConnectToMasterServer();
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    public void ConnectToMasterServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to master server.");
+        serverConnectScreen.SetActive(false);
+        mainMenu.SetActive(true);
+        connectionStatusText.SetText("Connected!");
+    }
+}
