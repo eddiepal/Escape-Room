3aac5bbb4b185b0875a50254d198fa459e61d949
Edward Burke <empburke@outlook.com>
2019-12-05 01:40:30 +0000

    Added player controller and camera controller, from tutorial project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Look Sensitivity")]
+    public float sensX; 
+    public float sensY;
+ 
+    [Header("Clamping")]
+    public float minY;
+    public float maxY;
+ 
+    private float rotX;
+    private float rotY;
+ 
+    
+    void Start ()
+    {
+        // lock the cursor to the middle of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    void LateUpdate ()
+    {
+        // get the mouse movement inputs
+        rotX += Input.GetAxis("Mouse X") * sensX;
+        rotY += Input.GetAxis("Mouse Y") * sensY;
+        
+        rotY = Mathf.Clamp(rotY, minY, maxY);
+        
+        // rotate the camera vertically
+        transform.localRotation = Quaternion.Euler(-rotY, 0, 0);
+ 
+        // rotate the player horizontally
+        transform.parent.rotation = Quaternion.Euler(transform.rotation.x, rotX, 0);
+    }
+}
