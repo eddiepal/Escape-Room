7992ad96369b78369abbc0ea53dcb79ce6311218
Edward Burke <empburke@outlook.com>
2019-12-08 01:45:28 +0000

    -Added a selection manager, for interacting with options such as the button

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -6,19 +6,42 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
 
+    [SerializeField] private Material defaultMaterial;
     [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag = "DoorButton";
+
+    private Transform _selection;
+    
     private void Update()
     {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        #region Selecting a Determination
+
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
+
+        #endregion
+            
         {
             var selection = hit.transform;
-            var selectionRenderer = selection.GetComponent<Renderer>();
-            if (selectionRenderer != null)
+            if (selection.CompareTag(selectableTag))
             {
-                selectionRenderer.material = highlightMaterial;
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
             }
+
         }
     }
 }
