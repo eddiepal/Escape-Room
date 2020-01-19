5f10c926b5b998037786ad08f407e4d7d91ec487
Edward Burke <empburke@outlook.com>
2020-01-19 11:43:37 +0000

    Returning to project work.
    Turned of selection manager for now. Muliplayer now working again.

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,48 +5,70 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
-
-    [SerializeField] private Material defaultMaterial;
-    [SerializeField] private Material highlightMaterial;
     [SerializeField] private string selectableTag = "DoorButton";
 
+    private HighlightSelectionResponse _selectionResponse;
+
     private Transform _selection;
-    
+
+    public Transform Selection
+    {
+        set { _selection = value; }
+        get { return _selection; }
+    }
+
     private void Update()
     {
         // Deselection/Selection Response
         if (_selection != null)
         {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            selectionRenderer.material = defaultMaterial;
-            _selection = null;
+            _selectionResponse.OnDeselect(_selection);
         }
 
-
+        #region Selection Determination
 
         // Creating a Ray
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         
         // Determining what was selected / Selection Determination
-
         _selection = null;
         if (Physics.Raycast(ray, out var hit))  
         {
             var selection = hit.transform;
-            // Deselection/Selection Response
+            
             if (selection.CompareTag(selectableTag))
             {
                 _selection = selection;
             }
         }
 
+        #endregion
+
+        // Deselection/Selection Response
         if (_selection != null)
         {
-            var selectionRenderer = _selection.GetComponent<Renderer>();
-            if (selectionRenderer != null)
-            {
-                selectionRenderer.material = highlightMaterial;
-            }
+            _selectionResponse.OnSelect(_selection);
         }
+    }
+}
+
+internal class HighlightSelectionResponse : MonoBehaviour
+{
+    [SerializeField] public Material defaultMaterial;
+    [SerializeField] public Material highlightMaterial;
+
+    public void OnSelect(Transform selection)
+    {
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = this.highlightMaterial;
+        }
+    }
+
+    public void OnDeselect(Transform selection)
+    {
+        var selectionRenderer = selection.GetComponent<Renderer>();
+        selectionRenderer.material = this.defaultMaterial;
     }
 }
