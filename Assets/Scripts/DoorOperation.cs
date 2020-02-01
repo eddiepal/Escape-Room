642249f565adc4f1e48a9dd5017e28b184f80a24
Edward Burke <empburke@outlook.com>
2020-02-01 12:11:34 +0000

    can now spell math to open door across the network!

---
+++ Assets/Scripts/DoorOperation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+
+public class DoorOperation : MonoBehaviour
+{
+    public float openSpeed = 5f;
+    public float closeSpeed = -5f;
+    private float originalPosition;
+    // Use this for initialization
+
+    private void Start()
+    {
+        originalPosition = transform.position.y;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.WordMade == true)
+        {
+            if (transform.position.y < originalPosition + 3)
+            {
+                //GetComponentInChildren<AudioSource>().Play();
+                float step = openSpeed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position,
+                    new Vector3(transform.position.x, 5.8f, transform.position.z), step);
+            }
+        }
+    }
+}
