435b61347e27e759dc6eec679b85ad1b1b758755
Edward Burke <empburke@outlook.com>
2020-02-09 12:27:16 +0000

    can now change word easily, still more work to do though, refactored if statements to use for loops, much cleaner code now

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -15,49 +15,32 @@ public class PressurePlate : MonoBehaviourPun
     public Material[] material;
     private Renderer rend;
     private List<GameObject> letterBoxesList;
+    [SerializeField] private List<GameObject> pressurePlates;
+    
 
     private void Start()
     {
         rend = GetComponent<MeshRenderer>();
         rend.enabled = true;
-        rend.sharedMaterial = material[0]; 
-        letterBoxesList = GetComponent<WordCreator>().LetterBoxes;
+        rend.sharedMaterial = material[0];
+         letterBoxesList = WordCreator.LetterBoxes;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-       
         if (GameManager.instance.WordMade == false)
         {
             if (other.gameObject.CompareTag("LetterBox"))
             {
-                Debug.Log("In OnCollisionEnter if statement");
                 rend.sharedMaterial = material[1];
             }
-            
-            if (gameObject.name == "PPM" && other.gameObject.name == letterBoxesList[0].name)
-            {
-                GameManager.instance.LetterPlaced[0] = true;
-            }
 
-            if (gameObject.name == "PPA" && other.gameObject.name == letterBoxesList[1].name)
+            for (int i = 0; i < letterBoxesList.Count; i++)
             {
-                GameManager.instance.LetterPlaced[1] = true;
-            }
-
-            if (gameObject.name == "PPT" && other.gameObject.name == letterBoxesList[2].name)
-            {
-                GameManager.instance.LetterPlaced[2] = true;
-            }
-
-            if (gameObject.name == "PPH" && other.gameObject.name == letterBoxesList[3].name)
-            {
-                GameManager.instance.LetterPlaced[3] = true;
-            }
-
-            if (gameObject.name == "PPS" && other.gameObject.name == letterBoxesList[4].name)
-            {
-                GameManager.instance.LetterPlaced[4] = true;
+                if (gameObject.name == pressurePlates[i].name && other.gameObject.name == letterBoxesList[i].name)
+                {
+                    GameManager.instance.LetterPlaced[i] = true;
+                }
             }
 
             if (AllLettersPlaced())
@@ -75,16 +58,13 @@ public class PressurePlate : MonoBehaviourPun
             rend.sharedMaterial = material[0];
         }
         
-        if (gameObject.name == "PPM" && gameObject.name == "LetterBox M")
-            GameManager.instance.LetterPlaced[0] = false;
-        if (other.gameObject.name == "LetterBox A")
-            GameManager.instance.LetterPlaced[1] = false;
-        if (other.gameObject.name == "LetterBox T")
-            GameManager.instance.LetterPlaced[2] = false;
-        if (other.gameObject.name == "LetterBox H")
-            GameManager.instance.LetterPlaced[3] = false;
-        if (other.gameObject.name == "LetterBox S")
-            GameManager.instance.LetterPlaced[4] = false;
+        for (int i = 0; i < letterBoxesList.Count; i++)
+        {
+            if (gameObject.name == pressurePlates[i].name && other.gameObject.name == letterBoxesList[i].name)
+            {
+                GameManager.instance.LetterPlaced[i] = false;
+            }
+        }
     }
 
     private bool AllLettersPlaced()
--- Assets/Scripts/WordCreator.cs
+++ Assets/Scripts/WordCreator.cs
@@ -8,6 +8,10 @@ using UnityEngine;
 public class WordCreator : MonoBehaviour
 {
     [SerializeField] private String theWord = "Test";
+    [SerializeField] private String letterBoxPrefab;
+    [SerializeField] private Transform[] letterSpawnPoint;
+
+    [SerializeField] private static List<GameObject> _letterBoxes = new List<GameObject>();
 
     public string TheWord
     {
@@ -15,31 +19,24 @@ public class WordCreator : MonoBehaviour
         set => theWord = value;
     }
     
-    public List<GameObject> LetterBoxes
+    public static List<GameObject> LetterBoxes
     {
-        get => letterBoxes;
-        set => letterBoxes = value;
+        get => _letterBoxes;
+        set => _letterBoxes = value;
     }
-
-    [SerializeField] private GameObject letterBoxPrefab;
-    [SerializeField] private Transform[] letterSpawnPoint;
-
-    [SerializeField] private List<GameObject> letterBoxes;
-
-
 
     private void Start()
     {
         for (int i = 0; i < theWord.Length; i++)
         {
-            GameObject letterBox = Instantiate(letterBoxPrefab, letterSpawnPoint[i].position, letterSpawnPoint[i].rotation);
-            letterBoxes.Add(letterBox);
+            GameObject letterBox = PhotonNetwork.Instantiate(letterBoxPrefab, letterSpawnPoint[i].position, letterSpawnPoint[i].rotation);
+            _letterBoxes.Add(letterBox);
         }
         
-        for (int i = 0; i < letterBoxes.Count; i++)
+        for (int i = 0; i < _letterBoxes.Count; i++)
         {
-            letterBoxes[i].name = "LetterBox" + theWord[i];
-            letterBoxes[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = theWord[i].ToString();
+            _letterBoxes[i].name = "LetterBox" + theWord[i];
+            _letterBoxes[i].transform.GetChild(0).GetComponent<TextMeshPro>().text = theWord[i].ToString();
         }
     }
 }
